6eb4626e6b7b14d72778f5331d96390f9eb8c896
Yaroslav Tsybukh <yaroslav.tsybukh@gmail.com>
2022-10-17 12:06:11 +0300

    i add lazy components

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,26 +1,31 @@
 import {BrowserRouter as Router , Route , Routes} from "react-router-dom";
+import {lazy , Suspense} from "react";
 
-import NotFoundPage from "../pages/404"
-import AppHeader from "../appHeader/AppHeader";
-import MainPage from "../pages/MainPage";
-import ComicsPage from "../pages/ComicsPage";
-import SingleComicPage from "../pages/SingleComicPage";
+const Spinner = lazy(() => import("../spinner/Spinner"))
+const NotFoundPage = lazy(() => import("../pages/404"))
+const AppHeader = lazy(() => import("../appHeader/AppHeader"))
+const MainPage = lazy(() => import("../pages/MainPage"))
+const ComicsPage = lazy(() => import("../pages/ComicsPage"))
+const SingleComicPage = lazy(() => import("../pages/SingleComicPage"))
+
 
 const App = () => {
     return (
-        <Router>
-            <div className="app">
-                <AppHeader/>
-                <main>
-                    <Routes>
-                        <Route path="/" element={<MainPage/>} />
-                        <Route path="/comics" element={<ComicsPage/>} />
-                        <Route path="/comics/:comicId" element={<SingleComicPage/>} />
-                        <Route path="*" element={<NotFoundPage/>} />
-                    </Routes>
-                </main>
-            </div>
-        </Router>
+        <Suspense fallback={<Spinner/>}>
+            <Router>
+                <div className="app">
+                    <AppHeader/>
+                    <main>
+                        <Routes>
+                            <Route path="/" element={<MainPage/>} />
+                            <Route path="/comics" element={<ComicsPage/>} />
+                            <Route path="/comics/:comicId" element={<SingleComicPage/>} />
+                            <Route path="*" element={<NotFoundPage/>} />
+                        </Routes>
+                    </main>
+                </div>
+            </Router>
+        </Suspense>
     )
 }
 
